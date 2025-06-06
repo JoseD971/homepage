4d66176cb9ecbe4557a7f987ea0ff8d09fa53347
JoseD971 <157433709+JoseD971@users.noreply.github.com>
2025-06-06 18:54:42 -0500

    feat: layout and styles of the About Me section
    feat: add images and icons folders

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
 import '../assets/style.css';
+import MainPhoto from '../assets/images/pexels-shvetsa-3727474.jpg';
+import GitHubIcon from '../assets/icons/github.svg';
+import LinkedlnIcon from '../assets/icons/linkedin.svg';
+import TwitterIcon from '../assets/icons/twitter.svg';
+
+document.getElementById('main-photo').setAttribute('src', MainPhoto);
+
+Array.from(document.getElementsByClassName('github-btn')).forEach(element => {
+    element.setAttribute('src', GitHubIcon);
+});
+
+Array.from(document.getElementsByClassName('linkedln-btn')).forEach(element => {
+    element.setAttribute('src', LinkedlnIcon);
+});
+
+Array.from(document.getElementsByClassName('twitter-btn')).forEach(element => {
+    element.setAttribute('src', TwitterIcon);
+});
