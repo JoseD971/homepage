2716631831c3352972d673969739cc9810cea953
JoseD971 <157433709+JoseD971@users.noreply.github.com>
2025-06-07 21:20:34 -0500

    feat: layout and styles of the Contact Me section

--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,12 @@ import GitHubIcon from '../assets/icons/github.svg';
 import LinkedlnIcon from '../assets/icons/linkedin.svg';
 import TwitterIcon from '../assets/icons/twitter.svg';
 import OpenNewTabIcon from '../assets/icons/open-in-new.svg';
+import FooterPhoto from '../assets/images/pexels-shvetsa-3727464.jpg';
+import PhoneIcon from '../assets/icons/phone-outline.svg';
+import EmailIcon from '../assets/icons/email-outline.svg';
 
 document.getElementById('main-photo').setAttribute('src', MainPhoto);
+document.getElementById('footer-photo').setAttribute('src', FooterPhoto);
 
 Array.from(document.getElementsByClassName('github-icon')).forEach(element => {
     element.setAttribute('src', GitHubIcon);
@@ -21,4 +25,12 @@ Array.from(document.getElementsByClassName('twitter-icon')).forEach(element => {
 
 Array.from(document.getElementsByClassName('open-new-tab-icon')).forEach(element => {
     element.setAttribute('src', OpenNewTabIcon);
+});
+
+Array.from(document.getElementsByClassName('phone-icon')).forEach(element => {
+    element.setAttribute('src', PhoneIcon);
+});
+
+Array.from(document.getElementsByClassName('email-icon')).forEach(element => {
+    element.setAttribute('src', EmailIcon);
 });
