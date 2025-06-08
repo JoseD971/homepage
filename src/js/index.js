a02e34e7ecbbb3f2878a0e84dddfe6fd57ac3e75
JoseD971 <157433709+JoseD971@users.noreply.github.com>
2025-06-07 19:47:24 -0500

    feat: layout and styles of the My Work section

--- src/js/index.js
+++ src/js/index.js
@@ -3,17 +3,22 @@ import MainPhoto from '../assets/images/pexels-shvetsa-3727474.jpg';
 import GitHubIcon from '../assets/icons/github.svg';
 import LinkedlnIcon from '../assets/icons/linkedin.svg';
 import TwitterIcon from '../assets/icons/twitter.svg';
+import OpenNewTabIcon from '../assets/icons/open-in-new.svg';
 
 document.getElementById('main-photo').setAttribute('src', MainPhoto);
 
-Array.from(document.getElementsByClassName('github-btn')).forEach(element => {
+Array.from(document.getElementsByClassName('github-icon')).forEach(element => {
     element.setAttribute('src', GitHubIcon);
 });
 
-Array.from(document.getElementsByClassName('linkedln-btn')).forEach(element => {
+Array.from(document.getElementsByClassName('linkedln-icon')).forEach(element => {
     element.setAttribute('src', LinkedlnIcon);
 });
 
-Array.from(document.getElementsByClassName('twitter-btn')).forEach(element => {
+Array.from(document.getElementsByClassName('twitter-icon')).forEach(element => {
     element.setAttribute('src', TwitterIcon);
+});
+
+Array.from(document.getElementsByClassName('open-new-tab-icon')).forEach(element => {
+    element.setAttribute('src', OpenNewTabIcon);
 });
